ddb222e0e31505947b44a5d8c83f46dce19be0ef
Hyun2l <kim12ho72@gmail.com>
2026-09-16 16:05:02 +0900

    Define columns

--- phase1_eda.py
+++ phase1_eda.py
@@ -3,4 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# RUL = Entire life of the engine (The last cycle) - Current cycle
+
+
 INDEX_COLS = ["unit", "cycle"]
+SETTING_COLS = ["op_setting1","op_setting2","op_setting3"]
+SENSOR_COLS = [f"sensor_{i}" for i in range(1,22)] # 6th -26th columns (n=21)
+ALL_COLS = INDEX_COLS + SETTING_COLS + SENSOR_COLS
